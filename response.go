fe8cf0c23131ab19a040f14c83dc8ba1d641a571
jokuskay <tafanasyev@gmail.com>
2018-07-01 00:14:17 +0700

    remove formatting logic

--- response.go
+++ response.go
@@ -1,10 +1,5 @@
 package emotions
 
-import (
-	"bytes"
-	"fmt"
-)
-
 // Face - Detected face
 type Face struct {
 	FaceId         string
@@ -98,36 +93,4 @@ type Hair struct {
 type HairColor struct {
 	Color      string
 	Confidence float32
-}
-
-// Get scores as String
-func (scores *Emotion) String() string {
-	var buffer bytes.Buffer
-
-	if anger := round(scores.Anger * 100); anger > 0 {
-		buffer.WriteString(fmt.Sprintf("Anger: %d%%\n", anger))
-	}
-	if contempt := round(scores.Contempt * 100); contempt > 0 {
-		buffer.WriteString(fmt.Sprintf("Contempt: %d%%\n", contempt))
-	}
-	if disgust := round(scores.Disgust * 100); disgust > 0 {
-		buffer.WriteString(fmt.Sprintf("Disgust: %d%%\n", disgust))
-	}
-	if fear := round(scores.Fear * 100); fear > 0 {
-		buffer.WriteString(fmt.Sprintf("Fear: %d%%\n", fear))
-	}
-	if happiness := round(scores.Happiness * 100); happiness > 0 {
-		buffer.WriteString(fmt.Sprintf("Happiness: %d%%\n", happiness))
-	}
-	if neutral := round(scores.Neutral * 100); neutral > 0 {
-		buffer.WriteString(fmt.Sprintf("Neutral: %d%%\n", neutral))
-	}
-	if sadness := round(scores.Sadness * 100); sadness > 0 {
-		buffer.WriteString(fmt.Sprintf("Sadness: %d%%\n", sadness))
-	}
-	if surprise := round(scores.Surprise * 100); surprise > 0 {
-		buffer.WriteString(fmt.Sprintf("Surprise: %d%%\n", surprise))
-	}
-
-	return buffer.String()
 }
